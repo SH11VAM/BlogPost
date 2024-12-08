3d7314185de04e627c3d55404fff1d5b6c7ae207
SH11VAM <jainshivam12527@gmail.com>
2024-12-08 14:27:22 +0530

    changes

--- src/components/Authlayout.jsx
+++ src/components/Authlayout.jsx
@@ -1,31 +1,24 @@
-import React from 'react'
-import { useState, useEffect} from 'react'
-import { useDispatch , useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import authservice from '../appwrite/auth'
+import React from "react";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import authservice from "../appwrite/auth";
 
-function Protected({children, authentication=true}) {
+function Protected({ children, authentication = true }) {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+  const authStatus = useSelector((state) => state.auth.status);
 
-    const navigate= useNavigate();
-    const [loading , setLoading]= useState(true);
-    const authStatus=useSelector( state => state.auth.status);
+  useEffect(() => {
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
 
-    useEffect(()=>{
-
-      if(authentication && authStatus !== authentication){
-        navigate("/login")
-      }else if(!authentication && authStatus !== authentication){
-navigate("/");
-
-      }
-
-      setLoading(false); 
-    }, [authStatus , navigate, authentication
-      
-    ]);
-  return (
-    loading ? <h1>.....loading..</h1>: <>{children}</>
-  )
+    setLoading(false);
+  }, [authStatus, navigate, authentication]);
+  return loading ? <h1>.....loading..</h1> : <>{children}</>;
 }
 
-export default Protected
+export default Protected;
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,8 @@ import Footer from "./footer/Footer";
 import Logo from "./Logo";
 import Button from "./Button";
 import Input from "./Input";
-
-export { Header,Footer , Button, Input, Logo}
+import Home from "../pages/Home"
+import AuthLayout from "../components/Authlayout"
+import Login from "../components/Login"
+ 
+export { Header,Footer , Button, Input, Logo, Home, AuthLayout, Login}
